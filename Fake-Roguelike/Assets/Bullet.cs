062333973b13ea6cc5f25a15545c98765a2e8528
LucielChroma <lucielchroma@gmail.com>
2021-08-19 23:26:22 -0300

    Primer Enemigo
    Agregué un enemigo simple capaz de seguir al jugador. Este desaparece al ser impactado por un proyectil.
    
    También hay pequeñas mejoras en la velocidad de los proyectiles, y ahora desaparecen unos segundos después de ser disparados.

--- Fake-Roguelike/Assets/Bullet.cs
+++ Fake-Roguelike/Assets/Bullet.cs
@@ -7,10 +7,10 @@ public class Bullet : MonoBehaviour
         public SpriteRenderer render;
         public float speed = 20f;
         public Rigidbody2D rb;
+        private int frames = 0;
 
     void Start()
     {
-
         if (Input.GetButton("Fire1"))
         {
             rb.velocity = transform.up * speed;
@@ -34,6 +34,16 @@ public class Bullet : MonoBehaviour
         else
         {
             render.GetComponent<Renderer>().sortingLayerID = SortingLayer.NameToID("Player Above");
+        }
+
+    }
+
+    void Update ()
+    {
+        frames++;
+        if (frames > 960)
+        {
+            Destroy(gameObject);
         }
 
     }
---
+++ Fake-Roguelike/Assets/DogAI.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogAI : MonoBehaviour
+{
+
+    public float speed, vision;
+    public Transform player;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float oldPosition = transform.position.x;
+        float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
+        if (distanceFromPlayer < vision)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
+        }
+
+        if (oldPosition > transform.position.x)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        } else
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        oldPosition = transform.position.x;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Bullet"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+}
