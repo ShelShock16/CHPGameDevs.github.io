08ab97c8fadfb6d9d920cb70504d990a48ddb460
LucielChroma <66520738+LucielChroma@users.noreply.github.com>
2021-10-28 06:46:55 -0300

    Salon Matemática creado
    falta mucho.

---
+++ Fake-Roguelike/Assets/MatGame.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatGame : MonoBehaviour
+{
+    public int[][] playerPosition;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Fake-Roguelike/Assets/MatPlayer.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatPlayer : MonoBehaviour
+{
+    public int playerX, playerY;
+    private float damageTime = 0, damageRedTime = 0, movementTime = 0;
+    public SpriteRenderer render;
+    void Start()
+    {
+        playerX = 6;
+        playerY = 9;
+    }
+
+    void Update()
+    {
+        if (movementTime == 0)
+        {
+            Movement();
+        }
+        if (movementTime > 0)
+        {
+            movementTime += Time.deltaTime;
+        }
+        if (movementTime > 0.20)
+        {
+            movementTime = 0;
+        }
+    }
+
+    void Damage()
+    {
+        if (damageTime > 0)
+        {
+            damageTime += Time.deltaTime;
+            damageRedTime += Time.deltaTime;
+            render.color = new Color(255, 0, 0, 128);
+            if (damageRedTime > 0.25 && damageRedTime < 0.5)
+            {
+                render.color = new Color(255, 255, 255, 255);
+            }
+            else if (damageRedTime > 0.5)
+            {
+                render.color = new Color(255, 0, 0, 128);
+                damageRedTime = 0;
+            }
+        }
+
+        if (damageTime > 1)
+        {
+            damageTime = 0;
+            render.color = new Color(255, 255, 255, 255);
+        }
+    }
+    void Movement()
+    {
+        if (Input.GetKey("a") && playerX > 1)
+        {
+            gameObject.transform.Translate(new Vector3(-0.24f, 0, 0));
+            movementTime += Time.deltaTime;
+            playerX--;
+        }
+
+        if (Input.GetKey("d") && playerX < 10)
+        {
+            gameObject.transform.Translate(new Vector3(0.24f, 0, 0));
+            movementTime += Time.deltaTime;
+            playerX++;
+        }
+
+        if (Input.GetKey("s") && playerY < 10)
+        {
+            gameObject.transform.Translate(new Vector3(0, -0.13f, 0));
+            movementTime += Time.deltaTime;
+            playerY++;
+        }
+
+        if (Input.GetKey("w") && playerY > 1)
+        {
+            gameObject.transform.Translate(new Vector3(0, 0.13f, 0));
+            movementTime += Time.deltaTime;
+            playerY--;
+        }
+    }
+}
